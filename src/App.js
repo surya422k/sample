fb9c05b697609b07f3f32d21f73781ca7808a8ce
surya <thorthunder2000@gmail.com>
2021-07-17 19:47:47 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>React Development</h2>
+        <h2>React Development And Deployment</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
